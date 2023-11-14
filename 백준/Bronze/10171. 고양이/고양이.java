792eb5b18377fca0e577f66deeeb03d0ac783a5b
ratcomp9992 <141790867+ratcomp9992@users.noreply.github.com>
2023-11-14 17:12:02 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bufw.write("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+
+        bufw.flush();
+        bufw.close();
+    }
+}
