53d28962d03dea1e8bc9a2247bf529fe7d1bd370
ratcomp9992 <141790867+ratcomp9992@users.noreply.github.com>
2023-11-14 14:03:18 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(bufr.readLine());
+        bufw.write (Integer.toString(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())));
+
+        bufw.flush();
+        bufw.close();
+        bufr.close();
+    }
+}
