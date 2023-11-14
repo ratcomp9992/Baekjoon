5291fa5cb42851d108cda37f14651589674a9db7
ratcomp9992 <141790867+ratcomp9992@users.noreply.github.com>
2023-11-14 16:41:14 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bufw.write(Integer.toString(Integer.parseInt(bufr.readLine()) - 543));
+
+        bufw.flush();
+        bufw.close();
+        bufr.close();
+    }
+}
