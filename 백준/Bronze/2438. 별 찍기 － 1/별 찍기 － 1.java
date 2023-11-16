d88bc6eac603635d31a7779cfbbac41eb91f5773
ratcomp9992 <141790867+ratcomp9992@users.noreply.github.com>
2023-11-16 18:27:37 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(bufr.readLine());
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j <= i; j++) {
+                bufw.write("*");
+            }
+            bufw.write('\n');
+        }
+
+        bufw.flush();
+        bufw.close();
+        bufr.close();
+    }
+}
