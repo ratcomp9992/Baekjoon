71252ab797e7c239ed2e992ad2eb483e6e0ebcd9
ratcomp9992 <141790867+ratcomp9992@users.noreply.github.com>
2023-11-16 18:03:06 +0900

    [Bronze V] Title: A+B - 3, Time: 136 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int T = Integer.parseInt(bufr.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(bufr.readLine());
+
+            bufw.write(Integer.toString(Integer.parseInt(st.nextToken()) +Integer.parseInt(st.nextToken())) + '\n');
+        }
+
+        bufw.flush();
+        bufw.close();
+        bufr.close();
+    }
+}
