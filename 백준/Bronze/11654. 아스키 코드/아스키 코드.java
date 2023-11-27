7a1f3a81c98375fb9a8e7cd8c605949c84485fc8
ratcomp9992 <141790867+ratcomp9992@users.noreply.github.com>
2023-11-27 18:00:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bufw.write(Integer.toString(bufr.readLine().charAt(0)));
+
+        bufw.flush();
+        bufw.close();
+        bufr.close();
+    }
+}
