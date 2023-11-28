db88fcc877d3861c8a7caa40e0865b70a8c47372
ratcomp9992 <141790867+ratcomp9992@users.noreply.github.com>
2023-11-28 18:46:33 +0900

    [Bronze II] Title: 단어의 개수, Time: 272 ms, Memory: 23700 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(bufr.readLine());
+
+        int cnt = 0;
+        while (st.hasMoreTokens()) {
+            st.nextToken();
+            cnt++;
+        }
+
+        bufw.write(Integer.toString(cnt));
+
+        bufw.flush();
+        bufw.close();
+        bufr.close();
+    }
+}
