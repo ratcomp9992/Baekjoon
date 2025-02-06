8bef2274ab1a63a3aec7376c0b1e82b35f317d19
ratcomp9992 <141790867+ratcomp9992@users.noreply.github.com>
2025-02-06 20:34:10 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(bufr.readLine());
+
+        bufw.write(Long.toString(n * n)+ "\n2");
+
+        bufw.flush();
+        bufw.close();
+        bufr.close();
+    }
+}
